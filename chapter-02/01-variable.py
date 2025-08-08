1215027b0698b867c3c19e693bd642ac5883f723
Anurag <anuragsharma921998@gmail.com>
2025-08-08 11:48:03 +0530

    Vaiables

--- chapter-02/01-variable.py
+++ chapter-02/01-variable.py
@@ -1,9 +1,7 @@
 name = "Anurag"
 age = 20
+price = 25.99
+city = "Panchkula"
 
-print (" My name is " ,name, "and My age is " ,age)
-
-# sum of a and b
-a = 20
-b = 20
-print (a + b)
+print("Hey My Name is", name ,"I'm from", city, ".")
+print(age)
