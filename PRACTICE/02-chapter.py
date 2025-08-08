fc400a055c853e8a022b09ab18d4da3fabe92c42
Anurag <anuragsharma921998@gmail.com>
2025-08-08 15:11:47 +0530

    python-Tasks

--- PRACTICE/02-chapter.py
+++ PRACTICE/02-chapter.py
@@ -2,3 +2,30 @@
 a = 10
 b = 20
 print(a + b)
+
+# TASK-2 [ WRITE A PROGRAM TO INPUT 2 NUMBERS & PRINT THERE SUM ]
+val1 = int(input("Enter first Value : "))
+val2 = int(input("Enter Second Value : "))
+sum = val1 + val2
+
+print("The Sum of two values is :", sum)
+
+# TASK-3 [ WAP TO INPUT SIDE OF A SQUARE & PRINT ITS AREA]
+side = float(input("Enter Side Of a Square :"))
+square = side * side
+print("The area of square is : ", square)
+
+# TASK-4 [WAP TO INPUT 2 FLOATING POINT NUMBERS & PRINT THEIR AVERAGE.]
+a = float(input("number 1 : "))
+b = float(input("number 2 : "))
+average = (a + b )/ 2
+print("The average of two values : ", average)
+
+""" 
+TASK-5 [ WAP TO INPUT 2 INT NUMBERS, A AND B.]
+PRINT TRUE IF A IS GREATER THAN OR EQUAL TO B. IF NOT PRINT FALSE.
+"""
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+
+print (a >= b,)
