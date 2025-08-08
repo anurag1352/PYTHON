53c2885ef8488644af8ba662346b0bbb04600d9b
Anurag <anuragsharma921998@gmail.com>
2025-08-08 13:11:00 +0530

    User Input

---
+++ chapter-02/05-input.py
@@ -0,0 +1,13 @@
+name = input("Enter your Name: ")
+print("Welcome!!", name)
+#print(type(name))
+
+# change to integer.
+age = int(input("Enter your age : "))
+print("Age is : ", age)
+print(type(age))
+
+# change to float
+price = float(input("what the price ? : "))
+print ("price is : ", price)
+print(type(price))
