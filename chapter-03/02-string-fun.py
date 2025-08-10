9cc580d3c6177fc003e5a146b99122ae6c54ceda
Anurag <anuragsharma921998@gmail.com>
2025-08-10 13:11:25 +0530

    String-Functions

--- chapter-03/02-string-fun.py
+++ chapter-03/02-string-fun.py
@@ -1,1 +1,16 @@
 # STRING FUNCTIONS.
+info = "i Anurag, and i learn python for devops."
+print(info.endswith("ops.")) # if end with ops. then print if not then print false.
+
+info_up = info.capitalize() # capitalize first or zero index.
+print(info_up)
+
+# REPLACING
+print(info.replace("i" ,"I'm"))
+
+# FIND
+print(info.find("Anurag"))
+print(info.find("p"))
+
+# COUNT.
+print(info.count("o"))
