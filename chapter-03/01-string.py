7060e0b2b100ffe9494d97b61f54bb0fdf8f8952
Anurag <anuragsharma921998@gmail.com>
2025-08-10 12:40:54 +0530

    String

---
+++ chapter-03/01-string.py
@@ -0,0 +1,8 @@
+# TYPES OF DECLARING STRINGS IN PYTHON
+str1 = 'Anurag' # single quotes.
+str2 = "Sharma" # double quotes.
+str3 = """Anurag Sharma""" # Triple quotes.
+
+print(str1)
+print(str2)
+print(str3)
