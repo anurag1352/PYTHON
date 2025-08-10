201d9641698066123e9533063304d1424989bf2b
Anurag <anuragsharma921998@gmail.com>
2025-08-10 18:30:41 +0530

    Chapter-03-Tasks

---
+++ PRACTICE/03-chapter.py
@@ -0,0 +1,10 @@
+# TASK.1
+#WAP TO INPUT USER'S FIRST NAME & PRINT IT'S  LENGTH.
+
+fname = input("Enter your first Name : ")
+print(len(fname))
+
+# TASK.2
+# WAP TO FIND THE OCCURENCE OF '$' IN A STRING.
+data = "In Inda , we use Ruppes & in USA use $."
+print(data.count("$"))
