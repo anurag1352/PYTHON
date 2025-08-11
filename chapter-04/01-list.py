b0a9670b86020404105c6746e72ad8f0fb5bd265
Anurag <anuragsharma921998@gmail.com>
2025-08-11 10:31:12 +0530

    List

---
+++ chapter-04/01-list.py
@@ -0,0 +1,18 @@
+# LIST IN PYTHON (SIMILAR TO ARRAY)
+marks = [33 , 77 , 75 , 68 , 49]
+print(marks[3])
+print(marks)
+print(type(marks))
+
+# === LIST OF STUDENT ===
+student = ["Anurag" , 85 , "Noida"]
+print("Before Edit : ", student)
+student[0] = "Angel"
+print("After Edit : ", student)
+
+# length of student
+print(len(student))
+
+ # === LIST SLICING ===
+num = [1,2,3,4,5,6,7]
+print(num[1:4])
