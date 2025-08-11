5c6b6b33220edb4756319ac9faf40bb4fde4f21e
Anurag <anuragsharma921998@gmail.com>
2025-08-11 22:00:04 +0530

    Chapter-04-Tasks

---
+++ PRACTICE/04-chapter.py
@@ -0,0 +1,16 @@
+# ==== CHAPTER TASKS =====
+
+# TASK .1
+# WAP TO ASK THE USER TO ENTER NAMES OF THEIR 3 FAVORITE MOVIES AND STORE THEN IN A LIST.
+movies = []
+movie1 = input("Enter First Favorite Movie : ")
+movie2 = input("Enter Second Favorite Movie : ")
+movie3 = input("Enter Third Favorite Movie : ")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+
+print(movies)
+
+# TASK .2
