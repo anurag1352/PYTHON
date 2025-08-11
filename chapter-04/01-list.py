474c856825c93a4666802b388443fa3fb966781c
Anurag <anuragsharma921998@gmail.com>
2025-08-11 10:38:37 +0530

    List

--- chapter-04/01-list.py
+++ chapter-04/01-list.py
@@ -16,3 +16,32 @@ print(len(student))
  # === LIST SLICING ===
 num = [1,2,3,4,5,6,7]
 print(num[1:4])
+
+# ==== LIST METHODS ====
+data = [7,5,6,4,3,2,1]
+print(data.append(8)) # add element at the end
+print(data)
+
+# Sorting = assending
+print(data.sort())
+print(data)
+
+# Sorting = desending
+print(data.sort(reverse=True))
+print(data)
+
+# Revere
+print(data.reverse())
+print(data)
+
+# INSERT
+print(data.insert(8,9))
+print(data)
+
+# REMOVE
+print(data.remove(9))
+print(data)
+
+# POP
+print(data.pop(7))
+print(data)
