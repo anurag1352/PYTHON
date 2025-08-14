1dc4a12993f0548039090c4a25b12d595a7b073e
Anurag <anuragsharma921998@gmail.com>
2025-08-14 18:40:27 +0530

    Directory-&-Set-task

--- PRACTICE/05-chapter.py
+++ PRACTICE/05-chapter.py
@@ -23,3 +23,28 @@ YOU ARE GIVEN A LIST OF SUBJECTS FOR STUDENTS.ASSUME ONE CLASSROOM IS REQUIRED F
 sub = {"PYTHON", "JAVA", "C++", "PYTHON", "JS",
 "JAVA", "PYTHON", "JAVA", "C++", "C"}
 print(len(sub))
+
+"""
+TASK 4.
+WAP to enter marks of 3 subjects from the user and store them in a dictionary. start with an empty dictionary & add one by one. use subject name as key & marks as value.
+"""
+
+# marks = {}
+
+# x = int(input("enter phy : "))
+# marks.update({"phy" : x})
+
+# y = int(input("enter chem : "))
+# marks.update({"chem" : y})
+
+# z = int(input("enter math : "))
+# marks.update({"math" : z})
+
+# print(marks)
+
+"""
+TASK .4
+figure out a way to store 9 & 9.0 as separate values in the set.
+"""
+store = {(9 , "int"), (9.0, "float")}
+print(store)
