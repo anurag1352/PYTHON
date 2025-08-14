c93d2f1922093211dd53227a86a5a418353a26ad
Anurag <anuragsharma921998@gmail.com>
2025-08-14 18:02:40 +0530

    Directory-&-Set

--- chapter-05/set.py
+++ chapter-05/set.py
@@ -5,6 +5,29 @@ set2 = {1,1,2,2,3,3} # repeted not print
 print(set2)
 print(set1)
 
+set2.add("hello")
+print(set2)
+
 # empty set
 khali = set()
 print(type(khali))
+
+#===== SET METHODS ======
+range = {1,2,3,"python",4,5,5,1,"hello"}
+print(range)
+print(range.add("Devops"))
+print(range)
+
+# remove element.
+range.remove(2)
+print(range)
+
+# delete random value.
+print(range.pop())
+print(range)
+
+#print(range[2]) # not allowed by index
+
+# clear set.
+range.clear()
+print(range)
