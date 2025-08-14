7c91165d5628b187e6b5a1e9d95bb85fe92f992e
Anurag <anuragsharma921998@gmail.com>
2025-08-14 17:06:12 +0530

    Directory-&-Set

---
+++ chapter-05/directory.py
@@ -0,0 +1,36 @@
+# ====== PYTHON DIRECTORY =======
+
+info = {
+    "name" : "Anurag",
+    "age" : 20,
+    "isAdult" : True,
+    "lang" : ["python", "JavaScript", "C"],
+    "topic" : ("dict","set")
+}
+
+print(info)
+print(info["lang"])
+print(info["isAdult"])
+
+# add/assign new value.
+info["name"] = "Anurag Sharma"
+print(info["name"])
+
+# delete key:value.
+del info["age"]
+print(info)
+
+# ===== NESTED DIRECTORY ======
+student = {
+    "name" : "Angel",
+    "score" : {
+        "chem" : 96,
+        "phy" : 88,
+        "bio" : "90"
+    }
+}
+
+print(student)
+print(student["name"])
+print(student["score"])
+print(student["score"] ["bio"]) # also access nested key:value.
