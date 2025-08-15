2de31b97a0c98dc15199edb1b0a8aa057f18b015
Anurag <anuragsharma921998@gmail.com>
2025-08-15 15:59:58 +0530

    Loops

---
+++ chapter-06/while-loop.py
@@ -0,0 +1,17 @@
+# ===== WHILE LOOP IN PYTHON ======
+count = 1
+
+while count <= 5:
+    count += 1
+    print("hello")
+
+# print counting
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+# infinite loop (don't use )
+# while True:
+#     print("Hello World")
