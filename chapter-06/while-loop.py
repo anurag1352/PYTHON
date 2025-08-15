905847663db9f7c682ac00e78700cc10825286cf
Anurag <anuragsharma921998@gmail.com>
2025-08-15 16:20:34 +0530

    Loops

--- chapter-06/while-loop.py
+++ chapter-06/while-loop.py
@@ -6,12 +6,42 @@ while count <= 5:
     print("hello")
 
 # print counting
-count = 5
+# count = 5
 
-while count >= 1:
-    print(count)
-    count -= 1
+# while count >= 1:
+#     print(count)
+#     count -= 1
 
 # infinite loop (don't use )
 # while True:
 #     print("Hello World")
+
+# print numbers 1 to 100.
+i = 1
+
+while i <= 100:
+    print(i)
+    i += 1
+
+# print 100 to 1.
+x = 100
+
+while x >= 1:
+    print(x)
+    x -= 1
+
+# Table print
+i = 1
+n = 3
+
+while i <= 10:
+    print(n * i)
+    i += 1
+
+# print square 1 to 10.
+a = 1
+
+while a <= 10:
+    print(a * a)
+    a += 1
+    
