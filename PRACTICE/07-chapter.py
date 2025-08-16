2d00e76b974a1b2bc972c950e78762ee6856cf18
Anurag <anuragsharma921998@gmail.com>
2025-08-16 16:08:52 +0530

    Functions-Tasks

--- PRACTICE/07-chapter.py
+++ PRACTICE/07-chapter.py
@@ -16,3 +16,21 @@ def print_list(list):
 
 print_list(hero)
 print_list(cities)
+
+# WAF TO FIND THE FACTORIAL OF N. (N IS THE PARAMETER)
+n = 5
+
+def cal_fact(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+        print(fact)
+
+cal_fact(5)
+
+# WAF TO CONVERT USD TO INR
+def converter(usd_val):
+    inr_val = usd_val * 83
+    print(usd_val, "USD =", inr_val, "INR")
+
+converter(8)
